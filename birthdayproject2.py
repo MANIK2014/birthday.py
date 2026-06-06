747125550d0d2e79fd5543e413307948cc03ec4c
MANIK2014 <tiwarisunu@gmail.com>
2026-06-06 15:43:16 +0100

    Add files via upload

---
+++ birthdayproject2.py
@@ -0,0 +1,14 @@
+john="Feburary 20th"
+david="July 16th"
+max="March 14th"
+peter="May 19th"
+hans="June 1st"
+
+print(john)
+print(david)
+print(max)
+print(peter)
+print(hans)
+
+#By writing their names you can know their birhdays!!
+#Note:All of them have the same birth year(1998)
